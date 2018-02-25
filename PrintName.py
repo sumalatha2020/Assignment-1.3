bc86af026f2f3cfa0664d2ccbe2053e375bc8871
sumalatha2020 <infosumalatha@gmail.com>
2018-02-25 15:52:23 +0530

    Add files via upload
    Assignment1.3 changes

---
+++ PrintName.py
@@ -0,0 +1,10 @@
+#Program to accept the first name and last name from user and print them in reverse order with space seperator
+
+#Accept the first name and store it in fname variable
+fname = input("Input your First Name : ")
+
+#Accept the Last name and store it in a lname variable
+lname = input("Input your Last Name : ")
+
+#Print in reverse order with space seperator
+print (lname + " " + fname)
